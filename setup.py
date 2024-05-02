1f7eacc2b99b260fa724860b60bcda0f09c6b704
Anri17 <andralmeida1999@gmail.com>
2024-05-02 16:41:45 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='drawing',
+      version='1.0',
+      description='Drawing Distribution Package',
+      author='André Almeida',
+      author_email='andralmeida1999@gmail.com',
+      url='https://github.com/Anri17/drawing',
+      packages=['drawing'],
+     )
